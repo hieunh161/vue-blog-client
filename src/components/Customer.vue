c25c61391372f03d324504a1d41638bb8e0080a8
Nguyễn Huy Hiệu <hieunh161@gmail.com>
2017-08-23 02:30:27 +0900

    add crud to customer

--- src/components/Customer.vue
+++ src/components/Customer.vue
@@ -11,6 +11,7 @@
           <th>Relationship</th>
           <th>Current Bill</th>
           <th>Revenue</th>
+          <th>Actions</th>
         </tr>
       </thead>
       <tbody>
@@ -20,27 +21,56 @@
           <td>{{customer.relationship}}</td>
           <td>{{customer.currentBill}}</td>
           <td>{{customer.revenue}}</td>
+          <td>
+            <div class="ui icon green button" @click="() => editCustomer(customer)"><i class="icon edit"></i></div>
+            <div class="ui icon red button" @click="() => removeCustomer(customer)"><i class="icon remove"></i></div>
+          </td>
         </tr>
       </tbody>
     </table>
-    <modal v-model="showModal">
-      <p slot="header">Confirmation needed</p>
-      <p slot="content">Do you want to continue?</p>
-      <template slot="actions">
-          <div class="ui black deny button" @click="showModal=false">
-            No
+    <div class="ui modal" id="editCustomer">
+      <i class="close icon"></i>
+      <div class="header">
+        Add Customer
+      </div>
+      <div class="content">
+        <form class="ui form">
+          <div class="field">
+            <label>Customer Name</label>
+            <input type="text" v-model="iCustomer.name" name="name" placeholder="Customer Name">
           </div>
-          <div class="ui positive right" @click="confirm">
-            Yes
+          <div class="two fields">
+            <div class="field" id="">
+              <label>Start Year</label>
+              <input type="number" v-model="iCustomer.startYear" name="startYear" placeholder="start year">
+            </div>
+            <div class="field">
+              <label>Relationship</label>
+              <input type="text" v-model="iCustomer.relationship" name="relationship" placeholder="Relationship">
+            </div>
           </div>
-      </template>
-  </modal>
+          <div class="two fields">
+            <div class="field">
+              <label>Current Bill (USD)</label>
+              <input type="text" v-model="iCustomer.currentBill" name="Current Bill" placeholder="Current Bill">
+            </div>
+            <div class="field">
+              <label>Revenue (USD)</label>
+              <input type="text" v-model="iCustomer.revenue" name="Revenue" placeholder="Revenue">
+            </div>
+          </div>
+        </form>
+      </div>
+      <div class="actions">
+        <div class="ui button basic red" @click="cancelForm">Cancel</div>
+        <div class="ui button green" id="updateButton" @click="submitForm">OK</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
-import modal from 'vue-semantic-modal';
 import Loader from './Loader';
 import CustomerTable from './CustomerTable';
 import CustomerForm from './CustomerForm';
@@ -52,39 +82,63 @@ export default {
   },
   data() {
     return {
-      showModal: true,
-      confirmed: true,
+      iCustomer: {},
+      selectedCustomer: null,
     };
   },
   methods: {
+    setSelectedCustomer(customer) {
+      this.selectedCustomer = customer;
+    },
+    getSelectedCustomer() {
+      return this.selectedCustomer;
+    },
     openModal() {
       /* global $:true */
       $('#editCustomer')
       .modal({ inverted: true })
       .modal('show');
     },
+    addCustomer(c) {
+      this.$store.dispatch('customer/addCustomer', c).then(
+        () => {
+          console.log('done');
+          $('#updateButton').removeClass('loading');
+          this.cancelForm();
+        },
+      );
+      this.iCustomer = {};
+    },
+    editCustomer(c) {
+      console.log(c.$key);
+    },
+    removeCustomer(c) {
+      console.log(c);
+      this.$store.dispatch('customer/removeCustomer', c);
+    },
     submitForm() {
-      $('#editCustomer')
-      .modal('hide');
+      $('#updateButton').addClass('loading');
+      this.addCustomer(this.iCustomer);
     },
     cancelForm() {
       $('#editCustomer')
       .modal('hide');
     },
-    confirm() {
-      this.confirmed = true;
-      this.showModal = false;
-    },
   },
   computed: {
     ...mapGetters({ customers: 'customer/customers' }),
     ...mapGetters({ isDoneLoading: 'customer/isDoneLoading' }),
+    ...mapGetters({ isUpdating: 'customer/isUpdating' }),
   },
   components: {
     Loader,
     CustomerTable,
     CustomerForm,
-    modal,
+  },
+  watch: {
+    // isUpdating(updating) {
+    //   console.log(`We have updating value ${updating} now, yaay!`);
+    // },
   },
 };
 </script>
--- src/store/customer.js
+++ src/store/customer.js
@@ -1,75 +1,124 @@
-const customerData = [
-  { name: 'FX',
-    startYear: 2012,
-    relationship: 'very good',
-    currentBill: '4M',
-    revenue: '100M',
-    logo: 'http://nsv.com/wp-content/uploads/2012/06/fuji-xerox-logo.png',
-    homepage: '',
-  },
-  { name: 'FXSS',
-    startYear: 2014,
-    relationship: 'good',
-    currentBill: '1M',
-    revenue: '100M',
-    logo: 'http://nsv.com/wp-content/uploads/2012/06/fuji-xerox-logo.png',
-    homepage: '',
-  },
-  { name: 'Canon',
-    startYear: 2016,
-    relationship: 'good',
-    currentBill: '1M',
-    revenue: '1B',
-    logo: 'http://www.canon.it/Images/Canon_Logo_350_tcm80-959888.jpg',
-    homepage: '',
-  },
-  { name: 'Sony',
-    startYear: 2012,
-    relationship: 'good',
-    currentBill: '4M',
-    revenue: '2B',
-    logo: 'https://lh4.googleusercontent.com/-G0mDI6fQXeM/AAAAAAAAAAI/AAAAAAAAfEI/q3w_lbwesr4/photo.jpg',
-    homepage: '',
-  },
-  { name: 'Konica',
-    startYear: 2017,
-    relationship: 'good',
-    currentBill: '1M',
-    revenue: '1B',
-    logo: 'https://www.konicaminolta.jp/about/release/minolta/org/img/konicaminolta_logo.gif',
-    homepage: '',
-  },
-  { name: 'SDNA',
-    startYear: 2011,
-    relationship: 'good',
-    currentBill: '0.5M',
-    revenue: '300M',
-    logo: 'https://lh4.googleusercontent.com/-G0mDI6fQXeM/AAAAAAAAAAI/AAAAAAAAfEI/q3w_lbwesr4/photo.jpg',
-    homepage: '',
-  },
-];
+import * as firebase from 'firebase';
+
+// const customerData = [
+//   { name: 'FX',
+//     startYear: 2012,
+//     relationship: 'very good',
+//     currentBill: '4M',
+//     revenue: '100M',
+//     logo: 'http://nsv.com/wp-content/uploads/2012/06/fuji-xerox-logo.png',
+//     homepage: '',
+//   },
+//   { name: 'FXSS',
+//     startYear: 2014,
+//     relationship: 'good',
+//     currentBill: '1M',
+//     revenue: '100M',
+//     logo: 'http://nsv.com/wp-content/uploads/2012/06/fuji-xerox-logo.png',
+//     homepage: '',
+//   },
+//   { name: 'Canon',
+//     startYear: 2016,
+//     relationship: 'good',
+//     currentBill: '1M',
+//     revenue: '1B',
+//     logo: 'http://www.canon.it/Images/Canon_Logo_350_tcm80-959888.jpg',
+//     homepage: '',
+//   },
+//   { name: 'Sony',
+//     startYear: 2012,
+//     relationship: 'good',
+//     currentBill: '4M',
+//     revenue: '2B',
+//     logo: 'https://lh4.googleusercontent.com/-G0mDI6fQXeM/AAAAAAAAAAI/AAAAAAAAfEI/q3w_lbwesr4/photo.jpg',
+//     homepage: '',
+//   },
+//   { name: 'Konica',
+//     startYear: 2017,
+//     relationship: 'good',
+//     currentBill: '1M',
+//     revenue: '1B',
+//     logo: 'https://www.konicaminolta.jp/about/release/minolta/org/img/konicaminolta_logo.gif',
+//     homepage: '',
+//   },
+//   { name: 'SDNA',
+//     startYear: 2011,
+//     relationship: 'good',
+//     currentBill: '0.5M',
+//     revenue: '300M',
+//     logo: 'https://lh4.googleusercontent.com/-G0mDI6fQXeM/AAAAAAAAAAI/AAAAAAAAfEI/q3w_lbwesr4/photo.jpg',
+//     homepage: '',
+//   },
+// ];
 
 /* eslint-disable no-param-reassign */
 const customer = {
   namespaced: true,
   state: {
     isLoading: true,
+    isUpdating: false,
     customers: null,
+    selectedCustomer: null,
   },
   mutations: {
     initCustomers: (s, payload) => {
       s.customers = payload.customers;
       s.isLoading = payload.isLoading;
     },
+    setSelectedCustomer: (s, c) => {
+      s.selectedCustomer = c;
+    },
   },
   actions: {
     loadCustomers: (context) => {
-      setTimeout(() => context.commit('initCustomers', { customers: customerData, isLoading: false }), 1000);
+      firebase.database().ref('customer').once('value', (snap) => {
+        const result = [];
+        snap.forEach((item) => {
+          const value = item.val();
+          value.key = item.key;
+          result.push(value);
+        });
+        context.commit('initCustomers', { customers: result, isLoading: false });
+      });
+    },
+    addCustomer: (context, c) => {
+      this.isUpdating = true;
+      firebase.database().ref('customer').push(c)
+      .then(
+        () => {
+          setTimeout(() => {
+            this.isUpdating = true;
+            console.log('push data success');
+          }, 3000);
+        },
+        (err) => {
+          this.isUpdating = true;
+          console.log(`push data err ${err}`);
+        },
+      );
+    },
+    removeCustomer: (context, c) => {
+      this.isUpdating = true;
+      firebase.database().ref(`customer/${c.key}`).remove()
+      .then(
+        () => {
+          setTimeout(() => {
+            this.isUpdating = true;
+            console.log('remove data success');
+          }, 3000);
+        },
+        (err) => {
+          this.isUpdating = true;
+          console.log(`remove data err ${err}`);
+        },
+      );
     },
   },
   getters: {
     customers: state => state.customers,
     isDoneLoading: state => !state.isLoading,
+    isUpdating: state => state.isUpdating,
+    selectedCustomer: state => state.selectedCustomer,
   },
 };
 
